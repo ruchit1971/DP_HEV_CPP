8776e1bf1aec2904573694f053c1a6692b9b9001
ruchit1971 <rucbh830@student.liu.se>
2021-05-22 11:11:03 +0200

    Add Drive Cycle Vector

--- main.cpp
+++ main.cpp
@@ -1,6 +1,27 @@
 #include <iostream>
+#include <vector>
+#include "City_MAN.h"
+
+
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+   std::vector<std::vector<int>> vec = { {1, 2, 3}, {4, 5, 6}, {7, 8, 9} };
+    //std::vector<std::vector<int>> vec(4, std::vector<int>(4));
+    std::cout << vec[0][1] << "\n";
+
+    std::vector<std::vector<double>> DC = City_MAN(); // T_z, V_z, G_z, D_z
+
+    std::vector<double> T_z = DC[0];
+    std::cout << T_z.size() << "\n";
+
+    for (double i : T_z){
+        std::cout << i << "\n";
+    }
+
+
+
+
+
+
     return 0;
 }
